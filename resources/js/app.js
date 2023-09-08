62a85112d1c0b05e09548903e7671b9248993c96
aalexebr <alex.ebr@icloud.com>
2023-09-08 16:03:09 +0200

    template

---
+++ resources/js/app.js
@@ -0,0 +1,10 @@
+import './bootstrap';
+
+import.meta.glob([
+    '../img/**'
+    ])
+
+// Import our custom CSS
+import '~resources/scss/app.scss'
+// Import all of Bootstrap's JS
+import * as bootstrap from 'bootstrap'
---
+++ resources/views/template-main.blade.php
@@ -0,0 +1,8 @@
+@extends('layout.layout-template')
+
+@section('content')
+{{-- code --}}
+<h1>
+    template main
+</h1>
+@endsection
